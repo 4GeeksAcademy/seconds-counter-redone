b9dbdee41d890a3dc4357848ed32a647eb14f972
Mai Tran <114268221+tranm98@users.noreply.github.com>
2023-05-02 23:08:16 +0000

    nan error fixed

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,80 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import rigoImage from "../../img/clock-512.png";
+
+
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [seconds, setSeconds] = useState(0);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setSeconds((seconds) => seconds + 1);
+    }, 1000);
+    return () => clearInterval(intervalId);
+  }, []);
+
+  const formatSeconds = (seconds) => {
+    const hours = Math.floor(seconds / 3600);
+    const minutes = Math.floor((seconds % 3600) / 60);
+    const remainingSeconds = seconds % 60;
+
+    let hoursStr = hours > 0 ? `${hours}:` : "";
+    let minutesStr = minutes.toString().padStart(2, "0");
+    let secondsStr = remainingSeconds.toString().padStart(2, "0");
+
+    return `${hoursStr}${minutesStr}:${secondsStr}`;
+  };
+
+  const secondsArr = formatSeconds(seconds)
+    .split("")
+    .filter((num) => num !== ":")
+    .map(Number);
+
+  const renderSeconds = () => {
+    if (seconds === 0) {
+      return (
+        <div key={0} className="seconds-box">
+          <img src={rigoImage} alt="clock" className="clock-img" />
+        </div>
+      );
+    }
+
+    const totalBoxes = 6;
+    const extraBoxes = totalBoxes - secondsArr.length;
+
+    let boxes = [];
+    for (let i = 0; i < extraBoxes; i++) {
+      boxes.push(
+        <div key={i} className="seconds-box">
+          0
+        </div>
+      );
+    }
+
+    boxes = boxes.concat(
+      secondsArr.map((num, index) => {
+        return (
+          <div key={index + extraBoxes} className="seconds-box">
+            {num}
+          </div>
+        );
+      })
+    );
+
+    return boxes;
+  };
+
+  return (
+    <div className="container">
+      <div className="clock-box">
+        <img src={rigoImage} alt="clock" className="clock-img" />
+      </div>
+      <div className="seconds-container">{renderSeconds()}</div>
+    </div>
+  );
 };
 
 export default Home;
--- src/js/index.js
+++ src/js/index.js
@@ -5,6 +5,7 @@ import ReactDOM from "react-dom";
 // include your styles into the webpack bundle
 import "../styles/index.css";
 
+
 //import your own components
 import Home from "./component/home.jsx";
 
